d146ef5b6b37ea8ff5f1078c7ee911336e15ac66
Sameer <sameenaraj01@gmail.com>
2026-05-25 22:08:21 +0530

    integrated supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
+import { createSupabaseClient } from "@/lib/supabase/server";
+import { Course } from "@/types/course";
+
+export default async function Home() {
+  const supabase = createSupabaseClient();
+
+  const { data: courses, error } = await supabase
+    .from("courses")
+    .select("*");
+
+  console.log(courses);
+  console.log(error);
+
   return (
-    <main className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
+    <main className="min-h-screen bg-black text-white p-10">
+      <h1 className="text-4xl font-bold mb-8">
         Student Dashboard
       </h1>
+
+      <pre>
+        {JSON.stringify(courses, null, 2)}
+      </pre>
     </main>
   );
 }
